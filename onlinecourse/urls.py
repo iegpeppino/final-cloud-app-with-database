b6603f621571216eb4b01e46029e5c5fc2052bac
iegpeppino <iegpeppino@outlook.com.ar>
2021-07-08 11:19:40 -0300

    Changes to template and manifest

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
-    path(route='', view=views.CourseListView.as_view(), name='index'),
+    path(route='', view= views.CourseListView.as_view(), name='index'),
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
